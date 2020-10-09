f9f347c184eab8f8a845640eba30866fd66ed95c
FaziletKosure <faziletkosure1@gmail.com>
2020-10-09 21:58:05 +0200

    updated

---
+++ listing_app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Fazilet bunu sevdi</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
